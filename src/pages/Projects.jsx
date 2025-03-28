35ffa67ed6d7092955c78442c1e6d7de3a1c4d3c
Muhammad Usman <usman820901@gmail.com>
2025-03-28 15:44:53 +0100

    filterData

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,33 +4,38 @@ import { Search } from '../components/Search';
 import { RiProgress4Line, RiProgress8Line } from 'react-icons/ri';
 
 const Projects = ({ projects }) => {
+  const filterData = projects.filter((item) => item.status === 'In progress');
+  console.log(filterData);
   return (
     <div className='bg-gray-100 p-4'>
       <Search />
-      {projects.map((project) => (
-        <div className=' justify-between p-4 border-[1px] shadow-sm border-l-4 border-gray-200 rounded-md mb-2 flex items-center bg-white  hover:border-[#63a27a] '>
-          <div className='flex flex-col leading-3 text-left'>
-            <p className='mb-4 font-semibold'>Project number</p>
+      {filterData.map((project) => (
+        <div className=' justify-between p-2 border-[1px] shadow-sm border-l-4 border-gray-200 rounded-md mb-2 flex items-center bg-white  hover:border-[#63a27a] '>
+          <div className=' flex flex-col  text-left'>
+            <p className='  font-semibold'>Project number</p>
             <p className=''>{project.projectNumber}</p>
           </div>
-          <div className='flex flex-col leading-3 text-left'>
-            <p className='mb-4 font-semibold'>Client name</p>
+          <div className='flex flex-col  text-left'>
+            <p className=' font-semibold'>Client name</p>
             <p className=''>{project.clientName}</p>
           </div>
-          <div className='flex flex-col leading-3 text-left'>
-            <p className='mb-4 font-semibold'>Customer invoice</p>
+          <div className='flex flex-col  text-left'>
+            <p className=' font-semibold'>Customer invoice</p>
             <p className=''>{project.customerInvoice}</p>
           </div>
-          <div className='flex flex-col leading-3 text-left'>
-            <p className='mb-4 font-semibold'>Supplier invoice</p>
+          <div className='flex flex-col  text-left'>
+            <p className=' font-semibold'>Supplier invoice</p>
             <p className=''>{project.supplierInvoice}</p>
           </div>
-          <div className='flex flex-col leading-3 text-left'>
-            <p className='mb-4 font-semibold'>Outcome</p>
+          <div className='flex flex-col text-left'>
+            <p className=' font-semibold'>Expected</p>
+            <p className=''>{project.expected}</p>
+          </div>
+          <div className='flex flex-col text-left'>
+            <p className=' font-semibold'>Outcome</p>
             <p className=''>{project.outcome}</p>
           </div>
-          <div className='flex flex-col leading-3 text-left'>
-            <p className='mb-4 font-semibold'>Status</p>
+          <div className='flex gap-2 flex-col leading-3 text-left'>
             <div className='flex items-center gap-2'>
               <p>{project.status}</p>
               <p>
@@ -41,12 +46,17 @@ const Projects = ({ projects }) => {
                 )}
               </p>
             </div>
-          </div>
-          <div className='flex flex-col leading-3 text-left gap-2'>
-            <label className='flex text-sm items-center selection:leading-3 text-left'>
-              <input type='checkbox' className='mr-2' />
-              Done
-            </label>
+            {project.status === 'Completed' ? (
+              <label className='flex text-sm gap-5 items-center selection:leading-3 text-left'>
+                In progress
+                <input type='checkbox' className='mr-2' />
+              </label>
+            ) : (
+              <label className='flex text-sm gap-5 items-center selection:leading-3 text-left'>
+                Completed
+                <input type='checkbox' className='mr-2' />
+              </label>
+            )}
             <Link
               to={`/projects/${project.id}`}
               key={project.id}
