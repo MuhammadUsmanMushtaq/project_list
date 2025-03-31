977911deb76dd37d518dc0a9421f2274b6b8cdba
Muhammad Usman <usman820901@gmail.com>
2025-03-31 20:21:45 +0200

    search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const Search = () => {
+export const Search = ({ data, onSearchResults }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (e) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+
+    const filteredData = data.filter(
+      (project) =>
+        project.clientName.toLowerCase().includes(term.toLowerCase()) ||
+        project.projectNumber.toString().includes(term)
+    );
+    onSearchResults(filteredData);
+  };
+
   return (
     <div className=' pt-8 pb-12 '>
       <form className='rounded-md bg-white p-6 shadow'>
         <div className='mb-2 flex items-center'>
           <input
             type='text'
+            value={searchTerm}
+            onChange={handleSearchChange}
             className='w-full rounded-md bg-gray-100 border border-gray-200 p-2'
-            placeholder='Search ...'
+            placeholder='Search by customer number or customer name...'
           />
           <button className='md:w-[120px] ml-2 rounded-md  bg-[#63a27a] p-2 text-white hover:bg-green-700'>
             Search
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Search } from '../components/Search';
 
 const Projects = ({ projects }) => {
+  const [filteredProjects, setFilteredProjects] = useState(projects);
+
+  const handleSearchResults = (results) => {
+    setFilteredProjects(results);
+  };
   return (
     <div className='max-w-6xl mx-auto bg-whiteshadow-md'>
-      <Search />
+      <Search data={projects} onSearchResults={handleSearchResults} />
       <div className='grid grid-cols-9 gap-4 bg-gray-100 font-medium p-3 rounded-t-md text-sm '>
         <div>Project #</div>
         <div>Cust Name</div>
@@ -17,50 +22,54 @@ const Projects = ({ projects }) => {
         <div>Details</div>
         <div>Actions</div>
       </div>
-      {projects.map((project) => (
-        <div key={project.id} className='divide-y divide-gray-300 '>
-          <div className='grid grid-cols-9 gap-4 p-2 items-center border-b border-x border-gray-100 hover:bg-gray-50 text-sm '>
-            <div>{project.projectNumber}</div>
-            <div>{project.clientName}</div>
-            <div>{project.customerInvoice}</div>
-            <div>{project.supplierInvoice}</div>
-            <div>{project.expected}</div>
-            <div>{project.outcome}</div>
-            {project.status === 'Completed' ? (
-              <div>
-                <span className='border-green-500 bg-green-50 text-green-500 font-medium border rounded-full px-2 py-1 text-center text-xs'>
-                  {project.status}
-                </span>
-              </div>
-            ) : (
-              <div>
-                <span className='border-orange-500 bg-orange-50 text-orange-400 font-medium border rounded-full px-2 py-1 text-center text-xs'>
-                  {project.status}
-                </span>
-              </div>
-            )}
-
-            <div>
-              <Link
-                to={`/projects/${project.id}`}
-                key={project.id}
-                className='text-blue-600 '
-              >
-                details
-              </Link>
-            </div>
-            <div>
+      {filteredProjects.length === 0 ? (
+        <p className='p-4 text-center border-x '>Oops No project found</p>
+      ) : (
+        filteredProjects.map((project) => (
+          <div key={project.id} className='divide-y divide-gray-300 '>
+            <div className='grid grid-cols-9 gap-4 p-2 items-center border-b border-x border-gray-100 hover:bg-gray-50 text-sm '>
+              <div>{project.projectNumber}</div>
+              <div>{project.clientName}</div>
+              <div>{project.customerInvoice}</div>
+              <div>{project.supplierInvoice}</div>
+              <div>{project.expected}</div>
+              <div>{project.outcome}</div>
               {project.status === 'Completed' ? (
-                <span>----</span>
+                <div>
+                  <span className='border-green-500 bg-green-50 text-green-500 font-medium border rounded-full px-2 py-1 text-center text-xs'>
+                    {project.status}
+                  </span>
+                </div>
               ) : (
-                <button className='px-2 py-1 bg-[#63a27a] text-white rounded'>
-                  Done
-                </button>
+                <div>
+                  <span className='border-orange-500 bg-orange-50 text-orange-400 font-medium border rounded-full px-2 py-1 text-center text-xs'>
+                    {project.status}
+                  </span>
+                </div>
               )}
+
+              <div>
+                <Link
+                  to={`/projects/${project.id}`}
+                  key={project.id}
+                  className='text-blue-600 '
+                >
+                  details
+                </Link>
+              </div>
+              <div>
+                {project.status === 'Completed' ? (
+                  <span>----</span>
+                ) : (
+                  <button className='px-2 py-1 bg-[#63a27a] text-white rounded'>
+                    Done
+                  </button>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
       <div className='border-x border-b rounded-b-md pt-8'>
         <div className='text-center mt-6 p-6 bg-gray-100'>pagination</div>
       </div>
